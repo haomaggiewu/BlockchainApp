dc14fce03a8264620c48d25f64fa3b7a87eff77e
WUHao <Maggie@WUHaodeMacBook-Pro.local>
2016-06-14 19:16:28 +0800

    Put the structure in.

---
+++ app/client/lib/ethereum/observeEvents.js
@@ -0,0 +1,60 @@
+// meant to speed up and make less requests
+var customContractsCache = {};
+/**
+Observe events
+
+@method observeEvents
+*/
+observeEvents = function(){
+    /**
+    Observe transactions, listen for new created transactions.
+
+    @class Events({}).observe
+    @constructor
+    */
+    collectionObservers[collectionObservers.length] = Events.find({}).observe({
+        /**
+        This will observe when events are added and link it to the custom contract.
+
+        @method added
+        */
+        added: function(newDocument) {
+            
+            // This creates a temporary cache for the contracts, to reduce the amount of db read and writes            
+            var customContract = customContractsCache[newDocument.address.toLowerCase()];
+
+            if (typeof customContract == 'undefined') { // not in cache, load from db
+                var customContract = CustomContracts.findOne({address: newDocument.address.toLowerCase()});
+                customContractsCache[newDocument.address.toLowerCase()] = customContract; 
+            }
+
+            if (typeof customContract != 'undefined' && typeof customContract.contractEvents != 'undefined') { // check if still undefined
+                // add to accounts
+
+                if (customContract.contractEvents && customContract.contractEvents.indexOf(newDocument._id)<0) {
+                    // Only if the event isn't there
+                    CustomContracts.update({address: newDocument.address.toLowerCase()}, {$addToSet: {
+                        contractEvents: newDocument._id
+                    }});  
+        
+                    customContract.contractEvents.push(newDocument._id);    
+                    customContractsCache[newDocument.address.toLowerCase()] = customContract; 
+                }  
+            }
+
+            
+
+        },
+        /**
+        Remove events confirmations from the accounts
+
+        @method removed
+        */
+        removed: function(document) {
+            CustomContracts.update({address: document.address.toLowerCase()}, {$pull: {
+                contractEvents: document._id
+            }});
+        }
+    });
+
+};
---
+++ app/client/templates/views/account.js
@@ -0,0 +1,289 @@
+/**
+Template Controllers
+
+@module Templates
+*/
+
+
+Template['views_account'].onRendered(function(){
+       console.timeEnd('renderAccountPage');
+});
+
+Template['views_account'].helpers({
+    /**
+    Get the current selected account
+
+    @method (account)
+    */
+    'account': function() {
+        return Helpers.getAccountByAddress(FlowRouter.getParam('address'));
+    },
+    /**
+    Get the current jsonInterface, or use the wallet jsonInterface
+
+    @method (jsonInterface)
+    */
+    'jsonInterface': function() {
+        return (this.owners) ? _.clone(walletInterface) : _.clone(this.jsonInterface);
+    },
+    /**
+    Get the pending confirmations of this account.
+
+    @method (pendingConfirmations)
+    */
+    'pendingConfirmations': function(){
+        return _.pluck(PendingConfirmations.find({operation: {$exists: true}, confirmedOwners: {$ne: []}, from: this.address}).fetch(), '_id');
+    },
+    /**
+    Return the daily limit available today.
+
+    @method (availableToday)
+    */
+    'availableToday': function() {
+        return new BigNumber(this.dailyLimit || '0', 10).minus(new BigNumber(this.dailyLimitSpent || '0', '10')).toString(10);  
+    },
+    /**
+    Show dailyLimit section
+
+    @method (showDailyLimit)
+    */
+    'showDailyLimit': function(){
+        return (this.dailyLimit && this.dailyLimit !== ethereumConfig.dailyLimitDefault);
+    },
+    /**
+    Show requiredSignatures section
+
+    @method (showRequiredSignatures)
+    */
+    'showRequiredSignatures': function(){
+        return (this.requiredSignatures && this.requiredSignatures > 1);
+    },
+    /**
+    Link the owner either to send or to the account itself.
+
+    @method (ownerLink)
+    */
+    'ownerLink': function(){
+        var owner = String(this);
+        if(Helpers.getAccountByAddress(owner))
+            return FlowRouter.path('account', {address: owner});
+        else
+            return FlowRouter.path('sendTo', {address: owner});
+    },
+    /**
+    Get all tokens
+
+    @method (tokens)
+    */
+    'tokens': function(){
+        var query = {};
+        query['balances.'+ this._id] = {$exists: true};
+        return Tokens.find(query, {sort: {name: 1}});
+    },
+    /**
+    Get the tokens balance
+
+    @method (formattedTokenBalance)
+    */
+    'formattedTokenBalance': function(e){
+        var account = Template.parentData(2);
+
+        return (this.balances && Number(this.balances[account._id]) > 0)
+            ? Helpers.formatNumberByDecimals(this.balances[account._id], this.decimals) +' '+ this.symbol
+            : false;
+    },
+    /**
+    Gets the contract events if available
+
+    @method (contractEvents)
+    */
+    'contractEvents': function(){
+        var customContract = CustomContracts.findOne({address: this.address.toLowerCase()});
+
+        return customContract ? customContract.contractEvents : null;
+    }
+});
+
+var accountClipboardEventHandler = function(e){
+    if (Session.get('tmpAllowCopy') === true) {
+        Session.set('tmpAllowCopy', false);
+        return true;
+    }
+    else {
+        e.preventDefault();
+    }
+
+    function copyAddress(){
+        var copyTextarea = document.querySelector('.copyable-address span');
+        var selection = window.getSelection();            
+        var range = document.createRange();
+        range.selectNodeContents(copyTextarea);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        try {
+            document.execCommand('copy');
+            
+            GlobalNotification.info({
+               content: 'i18n:wallet.accounts.addressCopiedToClipboard',
+               duration: 3
+            });
+        } catch (err) {
+            GlobalNotification.error({
+                content: 'i18n:wallet.accounts.addressNotCopiedToClipboard',
+                closeable: false,
+                duration: 3
+            });
+        }
+        selection.removeAllRanges();
+    }
+
+    if (Helpers.isOnMainNetwork()) {
+        Session.set('tmpAllowCopy', true);
+        copyAddress();
+    }
+    else {
+        EthElements.Modal.question({
+            text: new Spacebars.SafeString(TAPi18n.__('wallet.accounts.modal.copyAddressWarning')),
+            ok: function(){
+                Session.set('tmpAllowCopy', true);
+                copyAddress();
+            },
+            cancel: true,
+            modalQuestionOkButtonText: new Spacebars.SafeString(TAPi18n.__('wallet.accounts.modal.buttonOk')),
+            modalQuestionCancelButtonText: new Spacebars.SafeString(TAPi18n.__('wallet.accounts.modal.buttonCancel'))
+        });
+    }
+};
+
+Template['views_account'].events({
+    /**
+    Clicking the delete button will show delete modal
+
+    @event click button.delete
+    */
+    'click button.delete': function(e, template){
+        var data = this;
+
+        EthElements.Modal.question({
+            text: new Spacebars.SafeString(TAPi18n.__('wallet.accounts.modal.deleteText') + 
+                '<br><input type="text" class="deletionConfirmation" autofocus="true">'),
+            ok: function(){
+                if($('input.deletionConfirmation').val() === 'delete') {
+                    Wallets.remove(data._id);
+                    CustomContracts.remove(data._id);
+
+                    FlowRouter.go('dashboard');
+                    return true;
+                }
+            },
+            cancel: true
+        });
+    },
+    /**
+    Clicking the name, will make it editable
+
+    @event click .edit-name
+    */
+    'click .edit-name': function(e){
+        // make it editable
+        $(e.currentTarget).attr('contenteditable','true');
+    },
+    /**
+    Prevent enter
+
+    @event keypress .edit-name
+    */
+    'keypress .edit-name': function(e){
+        if(e.keyCode === 13)
+            e.preventDefault();
+    },
+    /**
+    Bluring the name, will save it
+
+    @event blur .edit-name, keyup .edit-name
+    */
+    'blur .edit-name, keyup .edit-name': function(e){
+        if(!e.keyCode || e.keyCode === 13) {
+            var $el = $(e.currentTarget);
+            var text = $el.text();
+
+
+            if(_.isEmpty(text)) {
+                text = TAPi18n.__('wallet.accounts.defaultName');
+            }
+
+            // Save new name
+            Wallets.update(this._id, {$set: {
+                name: text
+            }});
+            EthAccounts.update(this._id, {$set: {
+                name: text
+            }});
+            CustomContracts.update(this._id, {$set: {
+                name: text
+            }});
+
+            Tracker.afterFlush(function(argument) {
+                $el.text(text);
+            });
+
+            // make it non-editable
+            $el.attr('contenteditable', null);
+        }
+    },
+    /**
+    Click to copy the code to the clipboard
+    
+    @event click a.create.account
+    */
+    'click .copy-to-clipboard-button': accountClipboardEventHandler,
+
+    /**
+    Tries to copy account token.
+    
+    @event copy .copyable-address span
+    */
+    'copy .copyable-address': accountClipboardEventHandler,
+
+    /**
+    Click to reveal QR Code
+    
+    @event click a.create.account
+    */
+    'click .qrcode-button': function(e){
+        e.preventDefault();
+        
+        // Open a modal showing the QR Code
+        EthElements.Modal.show({
+            template: 'views_modals_qrCode',
+            data: {
+                address: this.address
+            }
+        });
+
+        
+    },
+    /**
+    Click to reveal the jsonInterface
+    
+    @event click .interface-button
+    */
+    'click .interface-button': function(e){
+        e.preventDefault();
+        var jsonInterface = (this.owners) ? _.clone(walletInterface) : _.clone(this.jsonInterface);
+        
+        //clean ABI from circular references
+        var cleanJsonInterface = _.map(jsonInterface, function(e, i) {
+            return _.omit(e, 'contractInstance');
+        })
+
+        // Open a modal showing the QR Code
+        EthElements.Modal.show({
+            template: 'views_modals_interface',
+            data: {
+                jsonInterface: cleanJsonInterface
+            }
+        });   
+    }
+});
